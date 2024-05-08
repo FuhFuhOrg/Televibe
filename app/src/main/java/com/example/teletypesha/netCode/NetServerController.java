85f252f89d3135d33f752c37a98368715af6930d
FuhFuh <ilyegorow@yandex.ru>
2024-05-08 21:39:18 +0300

    GetChatId has been added

--- app/src/main/java/com/example/teletypesha/netCode/NetServerController.java
+++ app/src/main/java/com/example/teletypesha/netCode/NetServerController.java
@@ -1,4 +1,4 @@
-package com.example.trpp_main_project.NetCode;
+package com.example.teletypesha.netCode;
 
 import android.app.Service;
 import android.content.Intent;
@@ -8,7 +8,8 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-import com.example.trpp_main_project.ItemControl.ReceptsItem;
+import com.example.teletypesha.itemClass.Chat;
+import com.example.teletypesha.itemClass.Messange;
 
 import java.io.Serializable;
 import java.net.URI;
@@ -188,7 +189,7 @@ public class NetServerController extends Service implements Serializable {
 
 
 
-//--------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 
 
 
@@ -197,7 +198,7 @@ public class NetServerController extends Service implements Serializable {
 
 
 
-    public CompletableFuture<String> GetId() {
+    public CompletableFuture<String> CreateNewChat() {
         CompletableFuture<String> future = new CompletableFuture<>();
         int requestId = GetK();
 
@@ -215,13 +216,13 @@ public class NetServerController extends Service implements Serializable {
         });
 
         Log.i("WebSocket", "SendMessage");
-        SendRequest(requestId, "GenerateUniqueUserId", "");
+        SendRequest(requestId, "ChatCreate", "");
 
         return future;
     }
 
-    public CompletableFuture<ArrayList<ReceptsItem>> SendMessage() {
-        CompletableFuture<ArrayList<ReceptsItem>> future = new CompletableFuture<>();
+    public CompletableFuture<ArrayList<Messange>> SendMessage() {
+        CompletableFuture<ArrayList<Messange>> future = new CompletableFuture<>();
         int requestId = GetK();
 
         setOnMessageReceivedListener(requestId , new OnMessageReceived() {
@@ -239,113 +240,6 @@ public class NetServerController extends Service implements Serializable {
 
         Log.i("WebSocket", "SendMessage");
         SendRequest(requestId, "GetRecomendsRecepts", "");
-
-        return future;
-    }
-
-    public CompletableFuture<ArrayList<ReceptsItem>> getRecomendsRecepts(){
-        CompletableFuture<ArrayList<ReceptsItem>> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-
-            }
-        });
-
-        Log.i("WebSocket", "Try get recepts");
-        SendRequest(requestId, "GetRecomendsRecepts", "");
-
-        return future;
-    }
-
-    public CompletableFuture<ArrayList<ReceptsItem>> GetRecepts(int from, int to){
-        CompletableFuture<ArrayList<ReceptsItem>> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", Arrays.toString(parts));
-                if (Objects.equals(parts[1], "true")) {
-                    // Преобразование в ArrayList и удаление первых двух элементов
-                    ArrayList arrayList = new ArrayList(Arrays.asList(parts));
-                    arrayList.remove(0);
-                    arrayList.remove(0);
-
-                    String joinedString = String.join(" ", arrayList);
-                    String[] splitArray = joinedString.split("_text_");
-                    ArrayList<String> resultList = new ArrayList<String>(Arrays.asList(splitArray));
-                    for (int i = 0; i < resultList.size(); i++){
-                        if (resultList.get(i).length() <= 1) {
-                            resultList.remove(i);
-                            i--;
-                        }
-                    }
-                    Log.i("WebSocket", "All Recepts Collect");
-
-
-                    ArrayList<ReceptsItem> receptsItems = new ArrayList<>();
-                    for (int i = 0; i < resultList.size(); i++){
-                        ArrayList<String> preClassArray = new ArrayList<String>
-                                (Arrays.asList(resultList.get(i).split(" . ")));
-                        int id = Integer.parseInt(preClassArray.get(0));
-                        String label = preClassArray.get(1);
-                        label = label.substring(3, label.length() - 4);
-                        ArrayList<String> products = new ArrayList<>
-                                (Arrays.asList(preClassArray.get(2).split(",")));
-                        ArrayList<String> users = new ArrayList<>
-                                (Arrays.asList(preClassArray.get(3).split(",")));
-                        ArrayList<String> tags = new ArrayList<>
-                                (Arrays.asList(preClassArray.get(4).split(",")));
-
-                        ReceptsItem receptsItem = new ReceptsItem
-                                (id, label, "-", -1, products, users, tags);
-                        receptsItems.add(receptsItem);
-                    }
-
-                    future.complete(receptsItems);
-                } else {
-                    future.complete(null);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try get recepts");
-        SendRequest(requestId, "GetRecepts", from + " " + to);
-
-        return future;
-    }
-
-    public CompletableFuture<ReceptsItem> GetRecept(ReceptsItem receptItem){
-        CompletableFuture<ReceptsItem> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId , new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", Arrays.toString(parts));
-                if (Objects.equals(parts[1], "true")) {
-                    // Преобразование в ArrayList и удаление первых двух элементов
-                    ArrayList arrayList = new ArrayList(Arrays.asList(parts));
-                    arrayList.remove(0);
-                    arrayList.remove(0);
-
-                    String joinedString = String.join(" ", arrayList);
-                    Log.i("WebSocket", "Recept Collect");
-
-                    receptItem.setText(joinedString);
-
-                    future.complete(receptItem);
-                } else {
-                    future.complete(null);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try get recept");
-        SendRequest(requestId, "GetRecept", String.valueOf(receptItem.getId()));
 
         return future;
     }
@@ -368,138 +262,6 @@ public class NetServerController extends Service implements Serializable {
 
         Log.i("WebSocket", "Try login");
         SendRequest(requestId, "Login", login + " " + password);
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> Like(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Like");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Like");
-        SendRequest(requestId, "Like", String.valueOf(id));
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> GetLike(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Like");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Get Like");
-        SendRequest(requestId, "IsLike", String.valueOf(id));
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> Dislike(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Dislike");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Dislike");
-        SendRequest(requestId, "Dislike", String.valueOf(id));
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> GetDislike(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Dislike");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Get Dislike");
-        SendRequest(requestId, "IsDislike", String.valueOf(id));
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> Favorite(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Favorite");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Favorite");
-        SendRequest(requestId, "Favorite", String.valueOf(id));
-
-        return future;
-    }
-
-    public CompletableFuture<Boolean> GetFavorite(int id) {
-        CompletableFuture<Boolean> future = new CompletableFuture<>();
-        int requestId = GetK();
-
-        setOnMessageReceivedListener(requestId, new OnMessageReceived() {
-            @Override
-            public void onMessage(String[] parts) {
-                Log.i("WebSocket", "Return Favorite");
-                if (Objects.equals(parts[1], "true")) {
-                    future.complete(true);
-                } else {
-                    future.complete(false);
-                }
-            }
-        });
-
-        Log.i("WebSocket", "Try Get Favorite");
-        SendRequest(requestId, "IsFavorite", String.valueOf(id));
 
         return future;
     }
