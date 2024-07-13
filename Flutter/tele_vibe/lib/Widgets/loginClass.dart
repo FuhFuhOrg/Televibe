f992d20d9684ef90200bfb1d30cbdb1e5e553191
FuhFuh <159549845+FuhFuhich@users.noreply.github.com>
2024-07-13 10:37:44 +0300

    Сделал дизайн для Login, мб нужно будет исправить высоту между текстфилдом и кнопками

--- Flutter/tele_vibe/lib/Widgets/loginClass.dart
+++ Flutter/tele_vibe/lib/Widgets/loginClass.dart
@@ -79,50 +79,91 @@ class _LoginClassState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text('Login'),
-        toolbarHeight: 200.0,
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _loginController,
-                decoration: const InputDecoration(
-                  labelText: 'Login',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () => _falseLoginAccount(context),
-                child: Text('Login'),
-              ),
-              SizedBox(height: 64),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () => _navigateToRegisterPage(context),
-                    child: Text('Go to Registration'),
+        backgroundColor: Colors.white,
+        title: const Center(
+          child: Text('Login'),
+          ),
+          toolbarHeight: MediaQuery.of(context).size.height * 0.25,
+          automaticallyImplyLeading: false,
+        ),
+      body: Form(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 8),
+                TextField(
+                  cursorColor: Colors.black,
+                  controller: _loginController,
+                  decoration: const InputDecoration(
+                    labelText: 'Login',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10.0),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10.0),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: MediaQuery.of(context).size.width * 0.523), // Это уродство, но мне в падлу менять, мб найду что-то на подобии constraint layout
+                ElevatedButton(
+                  onPressed: () => _falseLoginAccount(context),
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () => _navigateToRegisterPage(context),
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
+                      ),
+                      child: const Text(
+                        'Go to Registration',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- Flutter/tele_vibe/lib/Widgets/registrationClass.dart
+++ Flutter/tele_vibe/lib/Widgets/registrationClass.dart
@@ -49,151 +49,152 @@ class _RegisterClassState extends State<RegistrationPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Center(
-          child: Text('Login'),
+          child: Text('Registration'),
           ),
           toolbarHeight: MediaQuery.of(context).size.height * 0.25,
           automaticallyImplyLeading: false,
         ),
-      body: Center(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.9,// ограничение в 90% от краев экрана
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 8),
-              TextField(
-                cursorColor: Colors.black,
-                controller: _mailController,
-                decoration: const InputDecoration(
-                  labelText: 'Mail (необязательно)',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+      body: Form(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 8),
+                TextField(
+                  cursorColor: Colors.black,
+                  controller: _mailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Mail (необязательно)',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10.0),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  contentPadding: EdgeInsets.only(left: 10.0),
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                cursorColor: Colors.black,
-                controller: _loginController,
-                decoration: const InputDecoration(
-                  labelText: 'Login',
-                  labelStyle: TextStyle(color: Colors.black),
-                  border: OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+                const SizedBox(height: 8),
+                TextField(
+                  cursorColor: Colors.black,
+                  controller: _loginController,
+                  decoration: const InputDecoration(
+                    labelText: 'Login',
+                    labelStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red), 
+                    ),
+                    contentPadding: EdgeInsets.only(left: 10.0),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  errorBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red), 
-                  ),
-                  contentPadding: EdgeInsets.only(left: 10.0),
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                cursorColor: Colors.black,
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: const TextStyle(color: Colors.black),
-                  border: const OutlineInputBorder(),
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black), 
-                  ),
-                  errorBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red), 
-                  ),
-                  suffix: IconButton(
-                    icon: Icon(
-                      _obscureTextPassword ? Icons.visibility : Icons.visibility_off,
-                      color: Colors.black,
+                const SizedBox(height: 8),
+                TextField(
+                  cursorColor: Colors.black,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    border: const OutlineInputBorder(),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _obscureTextPassword = !_obscureTextPassword;
-                      });
-                    }
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black), 
+                    ),
+                    errorBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red), 
+                    ),
+                    suffix: IconButton(
+                      icon: Icon(
+                        _obscureTextPassword ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureTextPassword = !_obscureTextPassword;
+                        });
+                      }
+                    ),
+                    contentPadding: const EdgeInsets.only(left: 10.0),
                   ),
-                  contentPadding: const EdgeInsets.only(left: 10.0),
+                  obscureText: _obscureTextPassword,
                 ),
-                obscureText: _obscureTextPassword,
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                cursorColor: Colors.black,
-                controller: _rePasswordController,
-                decoration: InputDecoration(
-                  labelText: 'Re Password',
-                  labelStyle: const TextStyle(color: Colors.black),
-                  border: const OutlineInputBorder(),
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black), 
-                  ),
-                  errorBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  suffix: IconButton(
-                    icon: Icon(
-                      _obscureTextRePassword ? Icons.visibility : Icons.visibility_off,
-                      color: Colors.black,
+                const SizedBox(height: 8),
+                TextField(
+                  cursorColor: Colors.black,
+                  controller: _rePasswordController,
+                  decoration: InputDecoration(
+                    labelText: 'Re Password',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    border: const OutlineInputBorder(),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _obscureTextRePassword = !_obscureTextRePassword;
-                      });
-                    }
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black), 
+                    ),
+                    errorBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    suffix: IconButton(
+                      icon: Icon(
+                        _obscureTextRePassword ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureTextRePassword = !_obscureTextRePassword;
+                        });
+                      }
+                    ),
+                    contentPadding: const EdgeInsets.only(left: 10.0),
                   ),
-                  contentPadding: const EdgeInsets.only(left: 10.0),
+                  obscureText: _obscureTextRePassword,
                 ),
-                obscureText: _obscureTextRePassword,
-              ),
-              SizedBox(height: MediaQuery.of(context).size.width * 0.40),
-              ElevatedButton(
-                onPressed: () => _registrationNewAccount(context),
-                style: TextButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
+                SizedBox(height: MediaQuery.of(context).size.width * 0.25),
+                ElevatedButton(
+                  onPressed: () => _registrationNewAccount(context),
+                  style: TextButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
+                      ),
+                  child: const Text(
+                        'Register',
+                        style: TextStyle(color: Colors.black),
+                        
+                      )
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () => _navigateToLoginPage(context),
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
+                      ),
+                      child: const Text(
+                        'Go to Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
                     ),
-                child: const Text(
-                      'Registerr',
-                      style: TextStyle(color: Colors.black),
-                      
-                    )
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () => _navigateToLoginPage(context),
-                    style: TextButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(120, 160, 131, 1),
-                    ),
-                    child: const Text(
-                      'Go to Login',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
