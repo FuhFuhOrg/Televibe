57dfc66763cf361536ae9a06527acd5c31c4c709
SHA-inc-unity <85645846+SHA-inc-unity@users.noreply.github.com>
2025-01-28 12:01:44 +0300

    Чтото

--- Flutter/tele_vibe/lib/Widgets/UnderWidgets/fileUtils.dart
+++ Flutter/tele_vibe/lib/Widgets/UnderWidgets/fileUtils.dart
@@ -1,38 +1,38 @@
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
+/*import 'package:file_picker/file_picker.dart';*/
 
 class FileUtils {
   // Выбор фото
   static Future<File?> pickImage() async {
-    final result = await FilePicker.platform.pickFiles(
+    /*final result = await FilePicker.platform.pickFiles(
       type: FileType.image,
     );
     if (result != null && result.files.single.path != null) {
       return File(result.files.single.path!);
     }
-    return null;
+    return null;*/
   }
 
   // Выбор видео
   static Future<File?> pickVideo() async {
-    final result = await FilePicker.platform.pickFiles(
+    /*final result = await FilePicker.platform.pickFiles(
       type: FileType.video,
     );
     if (result != null && result.files.single.path != null) {
       return File(result.files.single.path!);
     }
-    return null;
+    return null;*/
   }
 
   // Выбор фото и видео
   static Future<File?> pickMedia() async {
-    final result = await FilePicker.platform.pickFiles(
+    /*final result = await FilePicker.platform.pickFiles(
       type: FileType.media,
     );
     if (result != null && result.files.single.path != null) {
       return File(result.files.single.path!);
     }
-    return null;
+    return null;*/
   }
 
 }
